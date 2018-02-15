bbef9536d9fbaa557dd87a887c9c16e703555735
Bidhu <bidhuprabha@gmail.com>
2018-02-15 13:14:33 +0530

    TestCommit

--- FizzBuzzSolution/FizzBuzz.Business/Constants.cs
+++ FizzBuzzSolution/FizzBuzz.Business/Constants.cs
@@ -9,5 +9,15 @@ namespace FizzBuzz.Business
     public  static class Constant
     {
         public const string Fizz = "Fizz";
+
+        public const string Buzz = "Buzz";
+
+        public const string FizzBuzz = "Fizz Buzz";
+
+        public const string Wizz = "Wizz";
+
+        public const string Wuzz = "Wuzz";
+
+        public const string WizzWuzz = "Wizz Wuzz";
     }
 }
